7f724e0233adbf2cb3cacb933329e2d9995660df
Jaskaran Gharial <gharial83@gmail.com>
2024-04-14 00:21:03 +0530

    Store Auth Users to Firebase Collections

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCJwE6jCO8D-Z4A5cTRSnEOU3nyf8ax3qQ",
@@ -19,3 +20,26 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async(userAuth) => {
+  const userDockRef = doc(db, 'users', userAuth.uid)
+  console.log(userDockRef)
+
+  const userSnapShot = await getDoc(userDockRef)
+  console.log(userSnapShot)
+
+  if(!userSnapShot.exists()) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDockRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log('Error creating the user', error)
+    }
+  }
+
+  return userDockRef
+}
