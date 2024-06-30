b83e6c8006cb056b9b7050a875eafe54f2d3af2f
Jaskaran Gharial <jaskaran.gharial@gmail.com>
2024-06-30 12:54:27 +0530

    Add Github Pages Deployment + Pre-Requisite Configuration

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
   return(
   <Routes>
-    <Route path='/' element={<Navigation />}>
+    <Route exact path='/' element={<Navigation />}>
       <Route index element={<Home />} />
       <Route path='/shop/*' element={<Shop />}/>
       <Route path='/auth' element={<Authentication />}/>
